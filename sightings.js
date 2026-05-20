301815b21478ff0f87767e9a08ebf9070dafc835
dakotadennison <dakotadennison@outlook.com>
2026-05-20 10:00:43 -0400

    Update sightings.js

--- sightings.js
+++ sightings.js
@@ -20,7 +20,7 @@ var sightings = [
   {
     id: 1,
     status: "pending",
-    creature: "MOTHMAN",
+    creature: "MOTHMANSEEEE",
     date: "MAY 12, 2026",
     location: "Chicago, Illinois, USA",
     context: "Witnessed a large winged figure hovering near the top of the Willis Tower at approximately 11:45pm. It did not move for several minutes before ascending vertically and disappearing. Three other people on the street saw it. No one had a good enough camera. The eyes were red and visible from at least 200 feet below.",
